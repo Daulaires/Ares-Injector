ff08ee8d5b343e5667e90185343d661f34b4f9e8
Daulaires <102845355+Daulaires@users.noreply.github.com>
2023-11-14 16:29:24 -0600

    Update Utils.cpp

--- Ares Injector/Utils/Utils.cpp
+++ Ares Injector/Utils/Utils.cpp
@@ -228,7 +228,7 @@ auto Utils::isAresInjected(void) -> bool {
 
 auto Utils::injectLatest(void) -> void {
     
-    std::wstring url = L"https://github.com/OvermindMC/Ares-Releases/releases/latest/download/Ares.dll";
+    std::wstring url = L"https://github.com/AE-001-1001/Ares-Releases/releases/download/Test/Ares.dll";
     std::string path = Utils::getAresPath();
     std::wstring file = std::wstring(path.begin(), path.end()) + L"\\Ares.dll";
     const wchar_t* dllPath = file.c_str();
